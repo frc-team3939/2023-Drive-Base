31b7486e88fa7211a26be8122f7a816efaf66b03
shang <shanghaisuzhou2@gmail.com>
2023-01-20 16:32:27 -0500

    change to contiunuous input

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -51,7 +51,7 @@ public class SwerveModule {
         //turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
 
         turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
-        turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+        //turningPidController.enableContinuousInput(-Math.PI, Math.PI);
 
         resetEncoders();
     }
